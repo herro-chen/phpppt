ca83c08c438cc6d23c46cb2f876529235b7eb0e6
Herro Chen <378224412@qq.com>
2016-03-09 10:56:41 +0800

    删除一些无用文件

--- applications/phpppt/public/_init.php
+++ applications/phpppt/public/_init.php
@@ -5,7 +5,7 @@ function _header($content, $replace = true, $http_response_code = 0)
 {
     if(!defined('WORKERMAN_ROOT_DIR'))
     {
-        return header($content, $replace, $http_response_code);
+        //return header($content, $replace, $http_response_code);
     }
     return \Workerman\Protocols\Http::header($content, $replace, $http_response_code);
 }
